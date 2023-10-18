1d761b04b1074ffb1e574401789ccb4378c3bcd7
Igor Ponomarev <9129910001@mail.ru>
2023-10-18 09:02:11 +0500

    added method 'post share' via email, added setting email and env.template

--- blog/mysite/blog/views.py
+++ blog/mysite/blog/views.py
@@ -2,6 +2,20 @@ from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+
+
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
+
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
 
 def post_list(request):
@@ -35,3 +49,26 @@ def post_detail(request: HttpRequest, year, month, day, post) -> HttpResponse:
     )
 
     return render(request, "./blog/post/detail.html", {"post": post})
+
+
+def post_share(request, post_id):
+    # Извлечь пост по индификатору id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == "POST":
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            # ... отправить электронное письмо
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, "9129910001@mail.ru", [cd["to"]])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )

fcc89b1931bd8102c6d76904ca335ae12022e0a6
Igor Ponomarev <9129910001@mail.ru>
2025-04-15 22:33:20 +0500

    Update views blog
    Add Tags to post, add similar tags, add search for title and body

--- blog/mysite/blog/views.py
+++ blog/mysite/blog/views.py
@@ -1,11 +1,15 @@
-from django.http import HttpRequest, HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404
-from .models import Post, Comment
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from django.contrib.postgres.search import TrigramSimilarity
 from django.core.mail import send_mail
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Count
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
+from django.views.generic import ListView
+from taggit.models import Tag
+
+from .forms import CommentForm, EmailPostForm, SearchForm
+from .models import Post
 
 
 class PostListView(ListView):
@@ -19,8 +23,12 @@ class PostListView(ListView):
     template_name = "blog/post/list.html"
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     # Постраничная разбивка  3 постами на страницу
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get("page", 1)
@@ -32,7 +40,14 @@ def post_list(request):
     except EmptyPage:
         # If page_number is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, "blog/post/list.html", {"posts": posts})
+    return render(
+        request,
+        "blog/post/list.html",
+        {
+            "posts": posts,
+            "tag": tag,
+        },
+    )
 
 
 def post_detail(request: HttpRequest, year, month, day, post) -> HttpResponse:
@@ -53,10 +68,22 @@ def post_detail(request: HttpRequest, year, month, day, post) -> HttpResponse:
     # Форма для комментирования пользователями
     form = CommentForm()
 
+    # Список схожих постов
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
+
     return render(
         request,
         "./blog/post/detail.html",
-        {"post": post, "comments": comments, "form": form},
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+            "similar_posts": similar_posts,
+        },
     )
 
 
@@ -100,4 +127,37 @@ def post_comment(request, post_id):
         request,
         "blog/post/comment.html",
         {"post": post, "form": form, "comment": comment},
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            # search_vector = SearchVector(
+            #     "title", weight="A", config="russian"
+            # ) + SearchVector("body", weight="B", config="russian")
+            # search_query = SearchQuery(query, config="russian")
+            # results = (
+            # Post.published.annotate(
+            # search=search_vector, rank=SearchRank(search_vector, search_query)
+            # )
+            # .filter(ank__gte=0.3)
+            # .order_by("-rank")
+            # )
+            results = (
+                Post.published.annotate(similarity=TrigramSimilarity("title", query))
+                .filter(similarity__gt=0.1)
+                .order_by("-similarity")
+            )
+
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
     )

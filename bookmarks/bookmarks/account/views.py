c76c36c5ffe7ba6df0eab30359843bf3cea2e390
Igor Ponomarev <9129910001@mail.ru>
2025-04-18 17:22:34 +0500

    Add login and logout

--- bookmarks/bookmarks/account/views.py
+++ bookmarks/bookmarks/account/views.py
@@ -1,8 +1,13 @@
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForms
+
+
+@login_required
+def dashboard(request):
+    return render(request, "account/dashboard.html", {"section": "dashboard"})
 
 
 def user_login(request):

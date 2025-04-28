0488ad6ff325ed590e0cfbcb96e9b46c3d56f02e
Igor Ponomarev <9129910001@mail.ru>
2025-04-28 22:19:42 +0500

    Added messages output for users when editing a profile

--- bookmarks/bookmarks/account/views.py
+++ bookmarks/bookmarks/account/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .forms import LoginForms, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
+from django.contrib import messages
 
 
 @login_required
@@ -61,6 +62,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, "Profile updated " "successfully")
+        else:
+            messages.error(request, "Error updating your profile")
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)

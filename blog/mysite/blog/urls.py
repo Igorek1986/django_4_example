1e7e49a1e6b785507e48dc7b4f9bacd24113aae4
Igorek1986 <51848581+Igorek1986@users.noreply.github.com>
2025-04-30 15:38:09 +0500

    Add 04 chapter
    * add .pylintc, .vscode, .idea to gitignore
    
    * Switch from Poetry to Uv dependency managment
    
    Add README file
    
    * update .gitignore
    
    * Change config
    
    Update ALLOWED_HOSTS, add new apps, change datebase to PostgrreSQL
    
    * Update urls
    
    Add url sitemaps
    
    * Update views blog
    
    Add Tags to post, add similar tags, add search for title and body
    
    * Feeds
    
    Add feeds
    
    * README
    
    Added description for chapter 1, 2, 3
    
    * Split projects and isolate their dependencies
    
    * Start project bookmarks
    
    * Add Django
    
    * Add python version
    
    * Add pyrightconfig for neovim
    
    * Add apps Account
    
    * Base migrate and account
    
    * Added .python-version.json in .gitignore
    
    * Add LoginForms
    
    * Add base.css
    
    * Add base.html
    
    * Add login.html
    
    * Add path login
    
    * Add path urlpatterns path the app account
    
    * Add function user_login
    
    * Add login and logout
    
    * Func registration
    
    Added func regitration new users.
    
    * refactor(auth): replace custom auth URLs with django.contrib.auth.urls
    
    - Remove custom name from auth URL patterns
    - Add all standard auth views:
      - LoginView
      - LogoutView
      - PasswordChangeView
      - PasswordChangeDoneView
      - PasswordResetView
      - PasswordResetDoneView
      - PasswordResetConfirmView
      - PasswordResetCompleteView
    - Replace individual paths with django.contrib.auth.urls include
    
    * Added templates/registration
    
    Added:
    - login.html
    - logged_out.html
    - password_change_done.html
    - password_change_form.html
    - password_re-set_confirm.html
    - password_re-set_done.html
    - password_reset_complete.html
    - password_reset_email.html
    - password_reset_form.html
    
    * Added templates/account
    
    Add templates:
    - register.html
    - register_done.html
    
    * Add template .env
    
    * Add form registration user
    
    In form added function to validation password
    
    * Added auth and email backend configuration
    
    - Set auth redirect URLs:
      - LOGIN_REDIRECT_URL = 'dashboard'
      - LOGIN_URL = 'login'
      - LOGOUT_URL = 'logout'
    - Configure email backend for development:
      - EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
    
    * Extending the User model
    
    Added Profile and fields:
    - date of birth
    - photo
    
    The __str__ Profile return's the username
    
    * Install Pillow
    
    * Added MEDIA_URL and MEDIA_ROOT
    
    * Add static files serving during development
    
    * Added migrations for Profile
    
    * Add user profile creation and editing functionality
    
    - Added automatic Profile creation during user registration
    - Added UserEditForm and ProfileEditForm for editing user and profile data
    - Implemented user profile editing view and template (edit.html)
    - Updated dashboard.html to include profile editing link or info
    
    * Added messages output for users when editing a profile
    
    * Added an email authentication method
    
    * Added email verification when registering and changing the email of an existing user
    
    * Added charter 4

--- blog/mysite/blog/urls.py
+++ blog/mysite/blog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import post_comment, post_detail, post_list, PostListView, post_share
+
+from .feeds import LatestPostsFeed
+from .views import post_comment, post_detail, post_list, post_search, post_share
 
 app_name = "blog"
 
 urlpatterns = [
-    # path("", post_list, name="post_list"),
-    path("", PostListView.as_view(), name="post_list"),
+    path("", post_list, name="post_list"),
+    # path("", PostListView.as_view(), name="post_list"),
+    path("tag/<slug:tag_slug>/", post_list, name="post_list_by_tag"),
     # path("<int:id>/", post_detail, name="post_detail"),
     path(
         "<int:year>/<int:month>/<int:day>/<slug:post>/",
@@ -14,4 +17,7 @@ urlpatterns = [
     ),
     path("<int:post_id>/share/", post_share, name="post_share"),
     path("<int:post_id>/", post_comment, name="post_comment"),
+    path("feed/", LatestPostsFeed(), name="post_feed"),
+    path("feeds/", LatestPostsFeed(), name="sss"),
+    path("search/", post_search, name="post_search"),
 ]
